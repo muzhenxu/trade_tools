0936b807b515c2fe68fc192173d1fa274bd9db7d
muzhenxu <muzhenxv@gmail.com>
2018-02-25 22:29:27 +0800

    update

--- monitor/alarm.py
+++ monitor/alarm.py
@@ -3,13 +3,14 @@ from rest_api.HuobiServices import *
 import pandas as pd
 import json
 import time
-
-itchat.auto_login(hotReload=True, enableCmdQR=2)
+from mailalarm.sendmail import send_mail
 
 xh_symbol = ['btcusdt']
 xh_period = ['5min']
 upper_threshold = 10200
-lower_threshold = 9600
+lower_threshold = 9400
+
+tid_bench = 0
 
 while True:
     for symbol in xh_symbol:
@@ -24,6 +25,12 @@ while True:
 
             df.id = pd.to_datetime(df.id.map(timestamp_to_datetime))
 
+            if df.id[0] == tid_bench:
+                time.sleep(10)
+                continue
+            else:
+                tid_bench = df.id[0]
+                
             amount_ratio = df.amount[1] / df.amount[2:].mean()
             upper = df.high[1]
             lower = df.low[1]
@@ -39,27 +46,29 @@ while True:
                 amount_msg = f'{symbol} {period} amount reaches {amount_ratio} times'
                 sgn += 1
             else:
-                amount_msg = amout_ratio
+                amount_msg = amount_ratio
 
             if upper >= upper_threshold:
                 upper_msg = f'{symbol} {period} high price is over {upper}'
                 sgn += 1
+                upper_threshold += 100
             else:
                 upper_msg = upper
 
             if lower <= lower_threshold:
                 lower_msg = f'{symbol} {period} low price reaches {lower}'
                 sgn += 1
+                lower_threshold -= 100
             else:
                 lower_msg = lower
 
-            if span >= 200:
+            if span >= 100:
                 span_msg = f'{symbol} {period} span is {span}'
                 sgn += 1
             else:
                 span_msg = span
 
-            if var >= 100:
+            if var >= 50:
                 var_msg = f'{symbol} {period} var is {var}'
                 sgn += 1
             else:
@@ -68,7 +77,7 @@ while True:
             msg = {'amount_msg': amount_msg, 'upper_msg': upper_msg, 'lower_msg': lower_msg, 'span_msg': span_msg, 'var_msg': var_msg}
 
             if sgn > 0:
-                itchat.send_msg(msg=json.dumps(msg), toUserName='muzhen')
+                send_mail(json.dumps(msg))
 
 
-            time.sleep(180)
+            time.sleep(10)
